119e7c2836fc57b48b1f797cffa24d97c16ff874
JoaoDajuda <joaopssoares607@gmail.com>
2026-09-08 11:27:57 -0300

    Parallax finalizado

--- parallax/src/pages/parallax/main.tsx
+++ parallax/src/pages/parallax/main.tsx
@@ -1,11 +1,25 @@
 import './parallax.css'
 import Mountain from '../../assets/mountain-removebg-preview.png'
+import { useEffect, useState } from 'react';
 
 function Parallax() {
-    
+        const [scrolling, setScroll] = useState(false);
+        useEffect(() => {
+            console.log("Scroll atual:", window.scrollY);
+            const handleScroll = () => {
+            if (window.scrollY > 10) {
+                setScroll(true);
+            } else {
+                setScroll(false);
+            }
+            };
+
+            window.addEventListener('scroll', handleScroll);
+            return () => window.removeEventListener('scroll', handleScroll);
+        }, []);
     return (
         <div className="parallax-outer">
-            <h1 className='title'> Parallax</h1>
+            <h1 className={`title ${scrolling ? 'scroll' : ''}`}> Parallax</h1>
             <img src={Mountain} alt='Mountainha' className='montanha'/>
         </div>
     )
